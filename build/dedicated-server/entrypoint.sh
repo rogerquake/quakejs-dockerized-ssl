5d10cc9b8c1e94939de6a231d5019d19aff1bf9f
rogerquake <roger@quakeunity.com>
2026-03-19 23:35:18 -0400

    Update entrypoint.sh

--- build/dedicated-server/entrypoint.sh
+++ build/dedicated-server/entrypoint.sh
@@ -4,10 +4,10 @@ while true
 do
     if [ "$Q3JS_SERVER_RESTART_HOURS" = "0" ]; then
         echo "[Dedicated Server] 🟢 Running indefinitely (restart disabled)."
-        node build/ioq3ded_secure.js +set fs_game "$Q3JS_SERVER_GAMEMODE" +set dedicated $Q3JS_SERVER_DEDICATED +set fs_cdn "$Q3JS_DOMAIN" +exec server.cfg
+        NODE_TLS_REJECT_UNAUTHORIZED=0 node build/ioq3ded_secure.js +set fs_game "$Q3JS_SERVER_GAMEMODE" +set dedicated $Q3JS_SERVER_DEDICATED +set fs_cdn "$Q3JS_DOMAIN" +exec server.cfg
     else
         echo "[Dedicated Server] 🟢 The server will be automatically restarted after $Q3JS_SERVER_RESTART_HOURS-hours."
-        timeout $Q3JS_SERVER_RESTART_SECONDS node build/ioq3ded_secure.js +set fs_game "$Q3JS_SERVER_GAMEMODE" +set dedicated $Q3JS_SERVER_DEDICATED +set fs_cdn "$Q3JS_DOMAIN" +exec server.cfg
+        NODE_TLS_REJECT_UNAUTHORIZED=0 timeout $Q3JS_SERVER_RESTART_SECONDS node build/ioq3ded_secure.js +set fs_game "$Q3JS_SERVER_GAMEMODE" +set dedicated $Q3JS_SERVER_DEDICATED +set fs_cdn "$Q3JS_DOMAIN" +exec server.cfg
     fi
     echo "[Dedicated Server] 🔴 Server stopped. Restarting..."
     sleep 5
